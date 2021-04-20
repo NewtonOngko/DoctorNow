18e3d4bb06439651d7cfea5282f84aab2f19be95
NewtonOngko <newton.ongko@gmail.com>
2021-04-21 00:26:55 +0700

    making stucture

---
+++ src/client/Containers/Login.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import '../../client/Styles/app.css';
+import ReactImage from '../../client/Assets/react.png';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {brand: "Ford"};
+  }
+  state = { username: null };
+
+  componentDidMount() {
+    fetch('/api/getUsername')
+      .then(res => res.json())
+      .then(user => this.setState({ username: user.username }));
+  }
+
+  render() {
+    const { username } = this.state;
+    return (
+      <div>
+        {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+        <img src={ReactImage} alt="react" />
+      </div>
+    );
+  }
+}
