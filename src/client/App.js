e5b08e639a4a7cc362fb80e284c0fa4083601171
NewtonOngko <newton.ongko@gmail.com>
2021-04-22 00:21:59 +0700

    making stucture

--- src/client/App.js
+++ src/client/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Login from '../../src/client/Containers/Login';
+import ReactDOM from "react-dom";
+import Main from "./Main";
 
 export default class App extends Component {
 
