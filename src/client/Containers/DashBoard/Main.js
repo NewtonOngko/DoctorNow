f63b2d785dff5a2846a823d44bd7945c989c6551
NewtonOngko <newton.ongko@gmail.com>
2021-06-02 22:31:16 +0700

    2 juni 2021 dashboard

--- src/client/Containers/DashBoard/Main.js
+++ src/client/Containers/DashBoard/Main.js
@@ -5,11 +5,17 @@ const useStyles = makeStyles({
     container: {
       width:'auto',
       height:'1060px',
-      backgroundColor: 'yellow',
+      backgroundColor: '#E5E5E5',
       display:'flex',
       flex:'1',
       flexDirection:'column'
     },
+    listitem:{
+      backgroundColor:'white',
+      width:'300px',
+      height:'100px',
+      margin:'20px'
+    }
   });
 
 export default function Main() {
@@ -19,9 +25,19 @@ export default function Main() {
       <div className={style.container} >
         <Header/>
         <p style={{fontSize:28,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Dashboard</p>
+         <div style={{display:'flex',flexDirection:'row'}}>
+         <div className={style.listitem}>
+            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >Total Users</p>
+         </div>
+         <div className={style.listitem}>
+            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Total Consultations</p>
+         </div>
+         <div className={style.listitem}>
+            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Total Doctors</p>
+         </div>
+         </div>
         </div>
       </>
-        
     )
 }
 
