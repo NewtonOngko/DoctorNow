ef051f7a34c35a73032c63404fbc76e0d0da543e
Nico <syahputranico906@gmail.com>
2021-06-04 21:50:06 +0700

    change configuration database to online

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,11 +1,11 @@
-const env = process.env;
+const { env } = process;
 
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || 'nicosps123',
-    database: env.DB_NAME || 'mydoctor',
+    host: env.DB_HOST || 'remotemysql.com',
+    user: env.DB_USER || 'gbSm65qWO0',
+    password: env.DB_PASSWORD || 'hI0rR4Y4vJ',
+    database: env.DB_NAME || 'gbSm65qWO0',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
