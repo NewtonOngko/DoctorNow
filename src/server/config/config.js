a7f5754974777d1a044666a56835deade9f8a9c4
William Handono <williamhandono@darkotech.id>
2021-06-11 11:49:23 +0700

    db no connection fix

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,6 +1,5 @@
 const { env } = process;
 const mysql = require('mysql');
-const q = require('q');
 var db_config={
   host: env.DB_HOST || 'remotemysql.com',
   user: env.DB_USER || 'gbSm65qWO0',
@@ -9,28 +8,8 @@ var db_config={
 }
 var config;
 function handleDisconnect(){
-  var d = q.defer();
-  config =mysql.createConnection(db_config);
-  config.connect((err) => {
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      d.reject
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    } 
-    else{
-      console.log('Database Connected!');
-      d.resolve(config)
-    }
-    return d.promise;
-  });
-  config.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
+  config =mysql.createPool(db_config);
+  console.log('running DB')
 }
 handleDisconnect();
 
