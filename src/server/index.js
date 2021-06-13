acc049c9b0ecc7898c92f51c884b0fa48a2f9b7f
NewtonOngko <newton.ongko@gmail.com>
2021-06-13 15:07:52 +0700

    adding cors

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,12 @@ const bodyParser = require('body-parser');
 
 // create express app
 const app = express();
-
+const cors = require('cors')
 // Setup server port
 const port = process.env.PORT || 5000;
+
+//cons permission
+app.use(cors('*'))
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
