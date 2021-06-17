0298a76e638e5fdf1af7eeebc29cb7ae019dc82d
Newton <newton.ongko@gmail.com>
2021-06-17 16:53:01 +0700

    done add user Ui upload havent done'

--- src/client/Containers/Users/MainAdd.js
+++ src/client/Containers/Users/MainAdd.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import Header from '../../Components/Header'
 import { DataGrid } from '@material-ui/data-grid';
@@ -8,6 +9,8 @@ import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
 import { blue, red } from '@material-ui/core/colors';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
 
 const useStyles = makeStyles({
@@ -37,127 +40,72 @@ const useStyles = makeStyles({
       border:0,
       padding:15,
       height:'400px'
-    }
+    },
+    
   });
-  const RowEdit = ({ index }) => {
-    const handleEditClick = () => {
-      // some action
-    };
-    return (
-      <FormControlLabel
-        control={
-          <>
-          <IconButton
-            color="secondary"
-            aria-label="add an alarm"
-            onClick={handleEditClick}
-          >
-            <EditIcon style={{ color: blue[500] }} />
-          </IconButton>
-          <IconButton
-            color="secondary"
-            aria-label="add an alarm"
-            onClick={handleEditClick}
-          >
-            <DeleteIcon style={{ color: red[500] }} />
-          </IconButton>
-          </>
-        }
-      />
-    );
-  };
 
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 100 },
-    { field: 'firstName', headerName: 'First name', width: 150 },
-    { field: 'lastName', headerName: 'Last name', width: 150 },
+  const Genderoption = [
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 110,
+      value: 'Male',
+      label: 'Male',
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.getValue(params.id, 'firstName') || ''} ${
-          params.getValue(params.id, 'lastName') || ''
-        }`,
-    },
-    {
-      field: "actions",
-      headerName: "Actions",
-      sortable: false,
-      width: 140,
-      disableClickEventBubbling: true,
-      renderCell: (params) => {
-        return (
-          <div
-            className="d-flex justify-content-between align-items-center"
-            style={{ cursor: "pointer" }}
-          >
-            <RowEdit index={params.row.id} />
-          </div>
-        );
-      }
+      value: 'Female',
+      label: 'Female',
     }
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 10, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    { id: 11, lastName: 'Wijaya', firstName: 'Gil', age: 65 },
-    { id: 12, lastName: 'Saputra', firstName: 'Nico', age: 65 },
-    { id: 13, lastName: 'Wong', firstName: 'SOI', age: 65 },
-    { id: 14, lastName: 'Gohza', firstName: 'HER', age: 65 },
   ];
 
 export default function MainAdd() {
     const style = useStyles()
+    const [gender, setGender] = React.useState('');
     return (
       <>
       <div className={style.container} >
         <Header/>
-        <p style={{fontSize:28,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Add Users</p>
-         {/* <div style={{display:'flex',flexDirection:'row'}}>
-         <div className={style.listitem}>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >Total Users</p>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >12</p>
-         </div>
-         <div className={style.listitem}>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Total Consultations</p>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >12</p>
-         </div>
-         <div className={style.listitem}>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Total Doctors</p>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >12</p>
-         </div>
-         </div> */}
-          <div>
+        <p style={{fontSize:28,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:20}}>Add Users</p>
           <div className={style.tablestyle}>
-            <div style={{padding:15,justifyContent:'flex-end',display:'flex'}}>
-            <Button
+          <p style={{fontSize:30,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>BASIC INFORMATION</p>
+          <Grid container direction="row" spacing ={2} style={{padding:20}}>
+              <Grid item xs ={6}>
+                <TextField fullWidth id="standard-required" label="Full Name"  />
+              </Grid>
+              <Grid item xs ={6}>
+                <TextField select fullWidth id="standard-select-currency" label="Gender" value={gender} onChange={e => setGender(e.target.value)}>
+                    {Genderoption.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                      {option.label}
+                    </MenuItem>
+                  ))}
+                </TextField>
+              </Grid>
+              <Grid item xs ={6}>
+                <TextField fullWidth id="standard-required" label="Date Of Birth" />
+              </Grid>
+              <Grid item xs ={6}>
+                <TextField fullWidth id="standard-required" label="BirthPlace"/>
+              </Grid>
+              <Grid item xs ={6}>
+                <TextField fullWidth id="standard-required" label="Email" />
+              </Grid>
+              <Grid item xs ={6}>
+                <TextField fullWidth id="standard-required" label="Occupation(optional)"/>
+              </Grid>
+              <Grid item xs ={12} sm={6} style={{justifyContent:'flex-start'}}>
+                <TextField fullWidth id="standard-required" label="Phone"/>
+              </Grid>
+          </Grid>
+              <Button
               variant="contained"
-              color="primary"
-              startIcon={<AddIcon />}>
-              Add Data
+              color="primary">
+              Save
               </Button>
-            </div>
-            <DataGrid className={style.data} rows={rows} columns={columns} pageSize={5} checkboxSelection />
+              <Button
+              variant="contained"
+              color="primary">
+              Cancel
+              </Button>
           </div>
-          </div>
-        </div>
+        </div>  
       </>
     )
 }
