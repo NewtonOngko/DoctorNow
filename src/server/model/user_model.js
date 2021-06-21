1274020de0e34e84bc69711c4fe4e4955e2f605a
NewtonOngko <newton.ongko@gmail.com>
2021-06-21 23:54:31 +0700

    encrypt password

--- src/server/model/user_model.js
+++ src/server/model/user_model.js
@@ -13,7 +13,7 @@ dotenv.config();
 const User = function userData(user) {
   this.full_name = user.full_name;
   this.email = user.email;
-  this.password = user.password;
+  this.password = generateHash(user.password);
   this.address = user.address;
   this.gender = user.gender;
   this.phone_number = user.phone_number;
@@ -129,6 +129,5 @@ User.login = function loginUser(req, res) {
     });
   }
 };
-
 
 module.exports = User;
