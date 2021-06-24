1b460dcca3094e0fcc77093c622190406ca75ddf
Nico <syahputranico906@gmail.com>
2021-06-24 21:57:43 +0700

    add route

--- src/client/Request/config.rest.js
+++ src/client/Request/config.rest.js
@@ -13,11 +13,35 @@ export default {
   API_URL: `${host}:${port01}`,
   REST: {
     auth: {
-      login: '/login',
+      login: '/login/admin',
     },
     user: {
       main: '/users',
       adduser :'/users'
+    },
+    doctor: {
+      main: '/doctors',
+      addDoctor :'/doctors'
+    },
+    hospital: {
+      main: '/hospitals',
+      addDoctor :'/hospitals'
+    },
+    consultation: {
+      main: '/consultations',
+      addConsultation :'/consultations'
+    },
+    appoitment: {
+      main: '/appoitments',
+      adduser :'/appoitments'
+    },
+    transactions: {
+      main: '/transactions',
+      adduser :'/transactions'
+    },
+    news: {
+      main: '/news',
+      adduser :'/news'
     }
   }
 }
