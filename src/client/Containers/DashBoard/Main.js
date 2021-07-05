a3dfa3eb0abe1a0d5ba8b70c4ab1ac4b45e728b5
Newton <newton.ongko@gmail.com>
2021-07-05 09:23:01 +0700

    total value

--- src/client/Containers/DashBoard/Main.js
+++ src/client/Containers/DashBoard/Main.js
@@ -9,6 +9,8 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import AddIcon from '@material-ui/icons/Add';
 import { blue, red } from '@material-ui/core/colors';
 import{GetUserAll}from '../../Request/service/users'
+import{GetConsultationAll}from '../../Request/service/consultation'
+import{GetDoctorAll}from '../../Request/service/doctor'
 import history from '../../Route/history'
 
 
@@ -142,6 +144,18 @@ export default function Main() {
          },
           )
           .catch((err)=> console.log(err))
+          GetConsultationAll()
+          .then((res)=> {
+            setTotalconst(res.length);
+           },
+            )
+            .catch((err)=> console.log(err))
+            GetDoctorAll()
+            .then((res)=> {
+              setTotaldoctors(res.length);
+             },
+              )
+              .catch((err)=> console.log(err))
       } 
     },[])
     return (
@@ -156,11 +170,11 @@ export default function Main() {
          </div>
          <div className={style.listitem}>
             <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Total Consultations</p>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >12</p>
+            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >{totalconst}</p>
          </div>
          <div className={style.listitem}>
             <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}}>Total Doctors</p>
-            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >12</p>
+            <p style={{fontSize:20,fontWeight:'bold',fontFamily: 'Noto Sans JP',margin:15}} >{totaldoc}</p>
          </div>
          </div>
           <div>
