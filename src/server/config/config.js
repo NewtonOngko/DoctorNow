2e1537a12368e96492a2f334a718927155be42f6
nicosps <syahputranico906@gmail.com>
2021-07-06 22:25:03 +0700

    revert db config

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,26 +1,18 @@
 const { env } = process;
 const mysql = require('mysql');
 
-// function handleDisconnect() {
-//   config = mysql.createPool(dbConfig);
-//   console.log('running DB');
-// }
-// handleDisconnect();
-
-var conn = mysql.createConnection({
+const dbConfig = {
   host: env.DB_HOST || 'remotemysql.com',
   user: env.DB_USER || 'gbSm65qWO0',
   password: env.DB_PASSWORD || 'TsiRHWudYt',
   database: env.DB_NAME || 'gbSm65qWO0',
-});
+};
+let config;
+function handleDisconnect() {
+  config = mysql.createPool(dbConfig);
+  console.log('running DB');
+}
+handleDisconnect();
 
-conn.connect(err => {
-  if (err) {
-    console.log('error connecting: ' + err.stack);
-    throw err
-  }
- 
-  console.log('connected as id ' + conn.threadId);
-});
 
-module.exports = conn;
+module.exports = config;
