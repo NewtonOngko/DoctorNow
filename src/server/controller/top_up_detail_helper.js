69bd0a4f6fac1981e0db474c28905027361dc9bd
nicosps <syahputranico906@gmail.com>
2021-07-17 14:30:45 +0700

    top up detail

--- src/server/controller/top_up_detail_helper.js
+++ src/server/controller/top_up_detail_helper.js
@@ -13,40 +13,76 @@ exports.findAll = function getTopUpDetail(req, res) {
 
 exports.create = function addTopUpDetail(req, res) {
   const newTopUpDetail = new TopUpDetail(req.body)
-  snap.transaction.notification(newTopUpDetail).then((statusResponse) => {
-    let orderId = statusResponse.order_id
-    let transactionStatus = statusResponse.transaction_status
-    let fraudStatus = statusResponse.fraud_status
-
-    console.log(
-      `Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`,
-    )
-
-    // Sample transactionStatus handling logic
-
-    if (transactionStatus == 'capture') {
-      if (fraudStatus == 'challenge') {
-        // TODO set transaction status on your database to 'challenge'
-        // and response with 200 OK
-      } else if (fraudStatus == 'accept') {
-        // TODO set transaction status on your database to 'success'
-        // and response with 200 OK
-      }
-    } else if (transactionStatus == 'settlement') {
-      // TODO set transaction status on your database to 'success'
-      // and response with 200 OK
-    } else if (
-      transactionStatus == 'cancel' ||
-      transactionStatus == 'deny' ||
-      transactionStatus == 'expire'
-    ) {
-      // TODO set transaction status on your database to 'failure'
-      // and response with 200 OK
-    } else if (transactionStatus == 'pending') {
-      // TODO set transaction status on your database to 'pending' / waiting payment
-      // and response with 200 OK
-    }
+  TopUpDetail.create(newTopUpDetail, (err, topUpDetail) => {
+    if (err) res.send(err)
+    res.json({
+      error: false,
+      message: 'Top Up added',
+      data: { topUpDetail },
+    })
   })
+  // snap.transaction.notification(newTopUpDetail).then((statusResponse) => {
+  //   let orderId = statusResponse.order_id
+  //   let transactionStatus = statusResponse.transaction_status
+  //   let fraudStatus = statusResponse.fraud_status
+
+  //   console.log(
+  //     `Transaction notification received. Order ID: ${orderId}. Transaction status: ${transactionStatus}. Fraud status: ${fraudStatus}`,
+  //   )
+
+  //   // Sample transactionStatus handling logic
+
+  //   if (transactionStatus == 'capture') {
+  //     if (fraudStatus == 'challenge') {
+  //       return res.status(400).send({ error: true, message: 'challenge' })
+
+  //       // TODO set transaction status on your database to 'challenge'
+  //       // and response with 200 OK
+  //     } else if (fraudStatus == 'accept') {
+  //       TopUpDetail.create(newTopUp, (err, topUp) => {
+  //         if (err) res.send(err)
+  //         res.json({
+  //           error: false,
+  //           message: 'Top Up added',
+  //           data: { topUp, transactionToken, transactionRedirectUrl },
+  //         })
+  //       })
+  //       // TODO set transaction status on your database to 'success'
+  //       // and response with 200 OK
+  //     }
+  //   } else if (transactionStatus == 'settlement') {
+  //     TopUpDetail.create(newTopUp, (err, topUp) => {
+  //       if (err) res.send(err)
+  //       res.json({
+  //         error: false,
+  //         message: 'Top Up added',
+  //         data: { topUp, transactionToken, transactionRedirectUrl },
+  //       })
+  //     })
+  //     // TODO set transaction status on your database to 'success'
+  //     // and response with 200 OK
+  //   } else if (
+  //     transactionStatus == 'cancel' ||
+  //     transactionStatus == 'deny' ||
+  //     transactionStatus == 'expire'
+  //   ) {
+  //     return res.status(400).send({ error: true, message: 'challenge' })
+
+  //     // TODO set transaction status on your database to 'failure'
+  //     // and response with 200 OK
+  //   } else if (transactionStatus == 'pending') {
+  //     TopUpDetail.create(newTopUp, (err, topUp) => {
+  //       if (err) res.send(err)
+  //       res.json({
+  //         error: false,
+  //         message: 'Top Up added',
+  //         data: { topUp, transactionToken, transactionRedirectUrl },
+  //       })
+  //     })
+  //     // TODO set transaction status on your database to 'pending' / waiting payment
+  //     // and response with 200 OK
+  //   }
+  // })
 }
 
 exports.findById = function getTopUpDetailById(req, res) {
